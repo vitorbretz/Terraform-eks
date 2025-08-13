3b9b85ea371820af8fddb2e084fdd45d90ba99f2
vitorbretz <vitorbretz28@gmail.com>
2025-08-13 18:35:44 -0300

    add provider

--- provider.tf
+++ provider.tf
@@ -1,13 +1,18 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.8.0"
     }
+  }
+  backend "s3" {
+    bucket = "terraform-learn-eks"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    
+  region = "us-east-1"
+
 }
