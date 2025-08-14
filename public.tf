f84e8f609677d7254dcca1e296e981d4571c2c8e
vitorbretz <vitorbretz28@gmail.com>
2025-08-14 15:44:21 -0300

    feat: new files with .tf

---
+++ public.tf
@@ -0,0 +1,23 @@
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  availability_zone       = "${data.aws_region.current.id}a"
+  map_public_ip_on_launch = true
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+
+  tags = {
+    Name                     = "learn-terraform-public-subnet-1a"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  availability_zone       = "${data.aws_region.current.id}b"
+  map_public_ip_on_launch = true
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+
+  tags = {
+    Name                     = "learn-terraform-public-subnet-1b"
+    "kubernetes.io/role/elb" = "1"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "cidr_block" {
+  type        = string
+  description = "value for the CIDR block of the VPC"
+
+
+}
