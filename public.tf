7415bd8641a498f8bdf2bbdaf0a52048343d8b10
vitorbretz <vitorbretz28@gmail.com>
2025-08-15 12:40:16 -0300

    feat: add news feats

--- public.tf
+++ public.tf
@@ -4,10 +4,13 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   map_public_ip_on_launch = true
   cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
 
-  tags = {
-    Name                     = "learn-terraform-public-subnet-1a"
-    "kubernetes.io/role/elb" = "1"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "learn-terraform-public-subnet-1a"
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
 }
 
 resource "aws_subnet" "eks_subnet_public_1b" {
@@ -16,8 +19,11 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   map_public_ip_on_launch = true
   cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
 
-  tags = {
-    Name                     = "learn-terraform-public-subnet-1b"
-    "kubernetes.io/role/elb" = "1"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name                     = "learn-terraform-public-subnet-1b"
+      "kubernetes.io/role/elb" = "1"
+    }
+  )
 }
