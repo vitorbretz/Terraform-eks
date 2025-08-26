8dad0d4e804f5303d27d3b022ece8d0526daa8ac
vitorbretz <vitorbretz28@gmail.com>
2025-08-26 20:18:31 -0300

    feat: add kubernetes provider

--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.8.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.38.0"
+    }
+
   }
   backend "s3" {
     bucket = "terraform-learn-eks"
@@ -15,6 +20,15 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 
+}
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
 
 
